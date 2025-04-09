fa39b9112d090e706bd3564e4920ad4cfd99b4d1
Luis8510 <luis.sch@web.de>
2025-04-09 21:39:45 +0200

    Final Version

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,10 @@ class FirstPortfolio extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          leading: const Icon(Icons.home),
+          leading: const Icon(
+            Icons.home,
+            color: Color(0xFF1C1B1F),
+          ),
           title: const Text(
             'First Portfolio Exam',
             style: TextStyle(color: Colors.white),
@@ -27,76 +30,83 @@ class FirstPortfolio extends StatelessWidget {
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
+                margin: const EdgeInsets.only(top: 20),
+                height: 100,
+                width: 200,
                 decoration: BoxDecoration(
                   color: const Color(0xffFF6A00),
-                  borderRadius: BorderRadius.circular(15),
+                  borderRadius: BorderRadius.circular(20),
                   border: Border.all(
                     color: Colors.black,
                     width: 5,
                   ),
                 ),
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
+                child: const Center(
                   child: Text(
                     'Welcome',
+                    overflow: TextOverflow.ellipsis,
                     style: TextStyle(fontSize: 40, color: Colors.white),
                   ),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: const Color(0xFF333333),
-                      ),
-                      const Text(
-                        'TAMK',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.w700),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: 100,
-                    height: 250,
-                    color: const Color(0xFF666666),
-                    child: const Center(
-                      child: Text(
-                        'Flutter!',
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: const Color(0xFF333333),
+                        ),
+                        const Text(
+                          'TAMK',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 100,
+                      height: 210,
+                      color: const Color(0xFF666666),
+                      child: const Center(
+                        child: Text(
+                          'Flutter!',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  Stack(
-                    alignment: Alignment.bottomLeft,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.grey,
-                      ),
-                      const Text(
-                        'THWS',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.w700),
-                      ),
-                    ],
-                  ),
-                ],
+                    Stack(
+                      alignment: Alignment.bottomLeft,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.grey,
+                        ),
+                        const Text(
+                          'THWS',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
               Container(
-                margin: const EdgeInsets.all(20),
+                margin: const EdgeInsets.only(bottom: 30),
+                height: 200,
+                width: 340,
                 decoration: BoxDecoration(
                   color: const Color(0xFF173C69),
                   border: Border.all(
